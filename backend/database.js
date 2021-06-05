130886b3c37ee9059ecb4b4f47e6333118ed9f1b
radhikaagarwal12 <85080989+radhikaagarwal12@users.noreply.github.com>
2021-06-05 23:16:05 +0530

    delete backend

--- backend/database.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-const connection = "mongodb+srv://radhika_12:radhika@1210@cluster0.o22tt.mongodb.net/dailyMailer?retryWrites=true&w=majority";
-mongoose.connect(connection,{ useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
-    .then(() => console.log("Database Connected Successfully"))
-    .catch(err => console.log(err));
-
